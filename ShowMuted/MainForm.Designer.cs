9e9b7d753438372f373e0578e523a32e58a800ee
Joxik <97808529+zJoxik@users.noreply.github.com>
2022-08-08 16:09:38 +0200

    Fix incorrect BackColor

--- ShowMuted/MainForm.Designer.cs
+++ ShowMuted/MainForm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.BackColor = System.Drawing.Color.Lime;
             this.ClientSize = new System.Drawing.Size(384, 311);
             this.MinimumSize = new System.Drawing.Size(150, 100);
             this.Name = "MainForm";
